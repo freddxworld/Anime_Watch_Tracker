47543374fb1978199b473e21c82663342e597c2b
freddxworld <98077586+freddxworld@users.noreply.github.com>
2025-11-11 18:17:58 -0800

    updated routes to public routes and protected routes

--- src/routes/animeRoutes.ts
+++ src/routes/animeRoutes.ts
@@ -10,24 +10,14 @@ import { authenticateToken } from "../middleware/authMiddleware";
 
 const router = Router();
 // public routes
-//post /anime --> create a new anime
-router.post("/", createAnime);
-
 //get /anime --> get all anime
 router.get("/", getAllAnime);
-
 //get /anime by it --> get one anime by id
 router.get("/:id", getAnimeById);
 
-//put /anime by id --> update anime by id
-router.put("/:id", updateAnime);
-
-//delete /anime by id --> delete anime by id
-router.delete("/:id", deleteAnime);
-
 // Protected routes
-router.post("/", authenticateToken, createAnime);
-router.put("/:id", authenticateToken, updateAnime);
-router.delete("/:id", authenticateToken, deleteAnime);
+router.post("/", authenticateToken, createAnime);   // Create new anime
+router.put("/:id", authenticateToken, updateAnime); // Edit anime
+router.delete("/:id", authenticateToken, deleteAnime); // Delete anime
 
 export default router;
